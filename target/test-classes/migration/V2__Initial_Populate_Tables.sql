f9373c458b5df98edb874ab6f655bf2242f81e9c
Jeremy <jeremiasrg@gmail.com>
2021-09-12 16:29:48 -0300

    part 8

---
+++ target/test-classes/migration/V2__Initial_Populate_Tables.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO `client` (`active`, `email`, `name` )
+VALUES (b'1', 'masterofthetests@test.com', 'Mr Tests');
+
+INSERT INTO `drink` (`active`, `name`, `price`) VALUES
+(b'1', 'Black Coffee', 4.0);
+INSERT INTO `drink` (`active`, `name`, `price`) VALUES
+(b'1', 'Latte', 5.0);
+INSERT INTO `drink` (`active`, `name`, `price`) VALUES
+(b'1', 'Mocha', 6.0);
+INSERT INTO `drink` (`active`, `name`, `price`) VALUES
+(b'1', 'Tea', 3.0);
+
+
+INSERT INTO `topping` (`active`, `name`, `price`) VALUES
+(b'1', 'Milk', 2.0);
+INSERT INTO `topping` (`active`, `name`, `price`) VALUES
+(b'1', 'Hazelnut syrup', 3.0);
+INSERT INTO `topping` (`active`, `name`, `price`) VALUES
+(b'1', 'Chocolate sauce', 5.0);
+INSERT INTO `topping` (`active`, `name`, `price`) VALUES
+(b'1', 'Lemon', 2.0);
+
+
+INSERT INTO `user` (`id`, `email`, `password`, `role`) VALUES
+(1, 'admin@admin.com', '$2a$10$0vX71kWJ6KSSzqi04Ie7rOD8.dBrKaLSbSjTrKexJlUJ9MbmqyyU6', 'ROLE_ADMINISTRATOR');
+
